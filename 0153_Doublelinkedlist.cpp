42fcd8e019b9e15d9608dfbcd2913f25f1c38e04
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-05-01 15:37:16 +0700

    tambah switch case menu

--- 0153_Doublelinkedlist.cpp
+++ 0153_Doublelinkedlist.cpp
@@ -234,3 +234,28 @@ int main()
         cout << "Enter your choice: ";
 
         cin >> choice;
+
+        switch (choice)
+        {
+        case '1':
+            list.addNode();
+            break;
+
+        case '2':
+            list.deleteNode();
+            break;
+
+        case '3':
+            list.traverse();
+            break;
+
+        case '4':
+            list.revtraverse();
+            break;
+
+        case '5':
+            list.searchData();
+            break;
+
+        case '6':
+            return 0;
