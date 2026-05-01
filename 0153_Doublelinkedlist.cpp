0523bffea36314cce35a21ae6718af6268b012c8
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-05-01 15:20:31 +0700

    tambah fungsi addNode untuk insert data ke double linked list

--- 0153_Doublelinkedlist.cpp
+++ 0153_Doublelinkedlist.cpp
@@ -22,3 +22,24 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "Enter the roll number of the student: ";
+        cin >> nim;
+
+        // Step 1: Allocate memory for new node
+        Node *newnode = new Node();
+
+        // Step 2: Assign value to the data field
+        newnode->no = nim;
+
+        // If list is empty or inserting at 1st is empty or not in sorted
+        if (START == NULL || nim <= START->no)
+        {
+            if (START != NULL && nim == START->no)
+            {
+                cout << "\nDuplicate roll numbers not allowed" << endl;
+                return;
+            }
